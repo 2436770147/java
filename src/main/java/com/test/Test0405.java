f768a77999c38273332ccd6407364e2bd7b7100b
2436770147 <2436770147@qq.com>
2023-05-13 20:55:48 +0800

    第49次提交

--- src/main/java/com/test/Test0405.java
+++ src/main/java/com/test/Test0405.java
@@ -9,14 +9,24 @@ public class Test0405 {
         System.out.println(a+" "+b);
 
         System.out.println();
-        System.out.println();
 
-        //Integer值在-128~127之间是值比较，Integer跟int也是值比较（自动拆装箱机制）
+        //Integer值在-128~127之间是值比较，Integer跟int也是值比较（自动拆箱机制）
         Integer i=-128;
         Integer j=-128;
         int x=-128;
+
+        //显式创建对象的方式会导致额外的开销，选择直接赋值int类型值给Integer（自动装箱机制）
+//        Integer n=new Integer(1);
+        Integer n=1;
+
         System.out.println(i==j);
         System.out.println(i==x);
+
+        System.out.println();
+
+        String str1="java";
+        String str2=new String(str1);
+        System.out.println(str1==str2);
     }
 
     private static void function(StringBuffer x, StringBuffer y) {
